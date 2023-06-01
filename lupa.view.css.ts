c3b71dac964b920324b0bcb31b4c08fd360f1e25
jin <jin@hyoo.ru>
2023-06-01 19:39:29 +0300

    More zoom

--- lupa.view.css.ts
+++ lupa.view.css.ts
@@ -2,8 +2,12 @@ namespace $.$$ {
 	
 	$mol_style_define( $hyoo_lupa, {
 		
+		overflow: 'hidden',
+		
 		Camera: {
 			objectFit: 'cover',
+			transform: 'scale(2)',
+			filter: 'contrast(1.25)',
 		},
 		
 	} )
