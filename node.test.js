01ab198b0ad46a0b248463ff772c5236cc89b420
koplenov <koplenov@users.noreply.github.com>
2023-11-11 07:07:07 +0000

    Deploying to gh-pages from @ koplenov/lupa.hyoo.ru@48e8fc9f99544c004ef3b9734e3a331c1e8cb053 🚀

--- node.test.js
+++ node.test.js
@@ -3939,7 +3939,12 @@ var $;
             }
             else {
                 this.native().setItem(key, JSON.stringify(next));
-                this.$.$mol_storage.persisted(true);
+                try {
+                    this.$.$mol_storage.persisted(true);
+                }
+                catch (error) {
+                    $mol_fail_log(error);
+                }
             }
             return next;
         }
