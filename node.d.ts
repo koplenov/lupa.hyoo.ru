08ee8a214d164b7cd9691a6800c0d9430651185e
koplenov <koplenov@users.noreply.github.com>
2024-04-03 07:08:03 +0000

    Deploying to gh-pages from @ koplenov/lupa.hyoo.ru@48e8fc9f99544c004ef3b9734e3a331c1e8cb053 🚀

--- node.d.ts
+++ node.d.ts
@@ -1278,7 +1278,7 @@ declare namespace $ {
 
 declare namespace $ {
 
-	type $mol_svg_path__geometry__N9RFGU1F = $mol_type_enforce<
+	type $mol_svg_path__geometry__4CIS7HEY = $mol_type_enforce<
 		ReturnType< $mol_icon['path'] >
 		,
 		ReturnType< $mol_svg_path['geometry'] >
@@ -1465,27 +1465,27 @@ declare namespace $ {
 //# sourceMappingURL=source.view.tree.d.ts.map
 declare namespace $ {
 
-	type $mol_video_camera__title__L1X5QKVL = $mol_type_enforce<
+	type $mol_video_camera__title__NLE8SXW3 = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_video_camera['title'] >
 	>
-	type $mol_video_camera__facing__B1V5IIV5 = $mol_type_enforce<
+	type $mol_video_camera__facing__2NBJCYYX = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_video_camera['facing'] >
 	>
-	type $mol_video_camera__sharpness__HBC3O8FY = $mol_type_enforce<
+	type $mol_video_camera__sharpness__T4EWSIL3 = $mol_type_enforce<
 		number
 		,
 		ReturnType< $mol_video_camera['sharpness'] >
 	>
-	type $mol_video_camera__torch__GQ1AV6B3 = $mol_type_enforce<
+	type $mol_video_camera__torch__7VY38DT4 = $mol_type_enforce<
 		boolean
 		,
 		ReturnType< $mol_video_camera['torch'] >
 	>
-	type $mol_link_source__uri__QGQY75KB = $mol_type_enforce<
+	type $mol_link_source__uri__7GOYPI7K = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_link_source['uri'] >
