e08afe0af384fdb71a8ca4f4712976693c4de8e3
koplenov <koplenov@users.noreply.github.com>
2024-04-18 07:08:57 +0000

    Deploying to gh-pages from @ koplenov/lupa.hyoo.ru@48e8fc9f99544c004ef3b9734e3a331c1e8cb053 🚀

--- web.d.ts
+++ web.d.ts
@@ -653,14 +653,21 @@ declare namespace $ {
             size?: (BG_size | [BG_size, BG_size])[];
         };
         box?: {
-            shadow?: readonly {
+            shadow?: readonly ([
+                ...[inset: 'inset'] | [],
+                x: Length,
+                y: Length,
+                blur: Length,
+                spread: Length,
+                color: $mol_style_properties_color
+            ] | {
                 inset?: boolean;
                 x: Length;
                 y: Length;
                 blur: Length;
                 spread: Length;
                 color: $mol_style_properties_color;
-            }[] | 'none' | Common;
+            })[] | 'none' | Common;
         };
         font?: {
             style?: 'normal' | 'italic' | Common;
@@ -1177,7 +1184,7 @@ declare namespace $ {
 
 declare namespace $ {
 
-	type $mol_svg_path__geometry__1BUY7BMZ = $mol_type_enforce<
+	type $mol_svg_path__geometry__HSQZZM0O = $mol_type_enforce<
 		ReturnType< $mol_icon['path'] >
 		,
 		ReturnType< $mol_svg_path['geometry'] >
@@ -1383,27 +1390,27 @@ declare namespace $ {
 //# sourceMappingURL=source.view.tree.d.ts.map
 declare namespace $ {
 
-	type $mol_video_camera__title__IH841XRK = $mol_type_enforce<
+	type $mol_video_camera__title__38DM030Q = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_video_camera['title'] >
 	>
-	type $mol_video_camera__facing__EAOHYEPI = $mol_type_enforce<
+	type $mol_video_camera__facing__WIS60PID = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_video_camera['facing'] >
 	>
-	type $mol_video_camera__sharpness__A5PNPJC6 = $mol_type_enforce<
+	type $mol_video_camera__sharpness__ZEVWJN0M = $mol_type_enforce<
 		number
 		,
 		ReturnType< $mol_video_camera['sharpness'] >
 	>
-	type $mol_video_camera__torch__OQPOHDRU = $mol_type_enforce<
+	type $mol_video_camera__torch__CJGSDIUH = $mol_type_enforce<
 		boolean
 		,
 		ReturnType< $mol_video_camera['torch'] >
 	>
-	type $mol_link_source__uri__M1G13IYS = $mol_type_enforce<
+	type $mol_link_source__uri__S84GKVYM = $mol_type_enforce<
 		string
 		,
 		ReturnType< $mol_link_source['uri'] >
